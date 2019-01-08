1efa877a6a94294896a8d4e98f357ab08e59330a
yanchengzhen <3020910a>
2019-01-08 15:18:14 +0800

    添加路径等

---
+++ pages/progress/progress.js
@@ -0,0 +1,23 @@
+// pages/progress/progress.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
